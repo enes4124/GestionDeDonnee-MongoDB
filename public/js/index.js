b0cbc518614bc507886eadb6fddb6ec56524feac
enes4124 <enes.karakaya5@etu.univ-lorraine.fr>
2022-12-06 17:44:47 +0100

    update

--- public/js/index.js
+++ public/js/index.js
@@ -1,72 +1,44 @@
-// The API Key is restricted to JSFiddle website
-// Get your own API Key on https://myprojects.geoapify.com
-const myAPIKey = "6dc7fb95a3b246cfa0f3bcef5ce9ed9a";
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
-// get a city with Geoapify Geocoding API - https://www.geoapify.com/geocoding-api
-const city = "Paris, France";
-document.getElementById('city').innerHTML = city;
+var marker = L.marker([51.5, -0.09]).addTo(map);
 
-// query places by categories. Find the complete list of the categories on https://apidocs.geoapify.com/docs/places/#categories
-const placeCategory = "commercial.books";
-document.getElementById('category').innerHTML = placeCategory;
+var circle = L.circle([51.508, -0.11], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 500
+}).addTo(map);
 
-let stopped = false;
-let results;
 
-document.getElementById('button').onclick = function() {
-  const state = document.getElementById('button').value;
-  if (state === "Start") {
-  	document.getElementById('button').value = "Stop";
-    getCityData(city, myAPIKey).then((cityData) => {
-      if (cityData) {
-        getAllPlaces(placeCategory, cityData.place_id, myAPIKey).then((places) => {
-        	results = places;
-          document.getElementById('button').value = "Download results";
-        });
-      } else {
-        console.log("The city is not found");
-      }
-    })
-  } else if (state === "Stop") {
-  	stopped = true;
-  } else {
-  	// Download results
-    const a = document.createElement('a');
-    const blob = new Blob([JSON.stringify(results)], {'type':'application\/json'});
-    a.href = window.URL.createObjectURL(blob);
-    a.download = `${placeCategory}-${city}-${(new Date()).toUTCString()}.json`;
-    a.click();
-  }
+var polygon = L.polygon([
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047]
+]).addTo(map);
+
+
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+
+
+var popup = L.popup()
+  .setLatLng([51.513, -0.09])
+  .setContent("I am a standalone popup.")
+  .openOn(map);
+
+
+
+function onMapClick(e) {
+  alert("You clicked the map at " + e.latlng);
 }
 
+map.on('click', onMapClick);
 
-function getCityData(city, apiKey) {
-  return fetch(`https://api.geoapify.com/v1/geocode/search?text=${encodeURIComponent(city)}&format=json&apiKey=${myAPIKey}`).then(data => data.json()).then(results => {
-    const cityData = results.results.length ? results.results[0] : null;
-    return cityData;
-  });
-}
-
-
-function getAllPlaces(category, cityId, apiKey) {
-  return getPlacesRecursively([], 0, category, cityId, apiKey);
-}
-
-// one Places API request returns maximum of 500 objects, so it may require making several API calls to get all the places
-function getPlacesRecursively(foundPlaces, offset, category, cityId, apiKey) {
-  // Set the maximal number of Places returned within one API call. Note more significant limit number will require a more significant processing time.
-  const limit = 20;
-  return fetch(`https://api.geoapify.com/v2/places?categories=${category}&filter=place:${cityId}&limit=${limit}&apiKey=${apiKey}&offset=${offset}`).then(data => data.json()).then(places => {
-    // Geoapify Places API returns a GeoJSON FeatureCollection object as a result. Let's collect only properties.
-    foundPlaces.push(...places.features.map(feature => feature.properties));
-
-    console.log(`${foundPlaces.length} places found`);
-
-    if (places.features.length === 0 || stopped) {
-      return foundPlaces;
-    } else {
-      // get the next page
-      return getPlacesRecursively(foundPlaces, offset + limit, category, cityId, apiKey);
-    }
-  });
-}
+console.log("Hello World");
--- src/init.php
+++ src/init.php
@@ -9,7 +9,7 @@ $pis = [];
 
 $db = (new MongoDB\Client('mongodb://mongo'))->selectDatabase('tdmongo');
 $data = json_decode(file_get_contents('https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Parking/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=nom%2Cadresse%2Cplaces%2Ccapacite&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson'));
-$db->createCollection('pis');
+// $db->createCollection('pis');
 $db = $db->selectCollection('pis');
 
 foreach ($data->features as $feature) {
@@ -32,3 +32,5 @@ foreach ($data->features as $feature) {
 if (count($pis) > 0) {
   $res = $db->insertMany($pis);
 }
+
+var_dump($pis);
